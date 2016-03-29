3d02009b4d493691681c60db7d651a4d10f24034
jack <746560359@qq.com>
2016-03-29 12:37:32 +0800

    修改ip配置

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 """
 命令行运行：python manage.py runserver
 其他电脑访问：
-python manage.py runserver localhost:8000
+python manage.py runserver 0.0.0.0:8000
 
 同步修改数据库1.9
 python manage.py makemigrations
